283cf433023fd21f3d88b66a2e4680fe66d45803
Boba Fett <thomascupchurch@gmail.com>
2021-08-17 18:05:20 -0500

    post method

--- server.js
+++ server.js
@@ -64,6 +64,12 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.post('/api/animals', (req, res) => {
+  // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
